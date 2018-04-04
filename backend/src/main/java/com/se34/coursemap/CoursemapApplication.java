ca30dc5ffc42d6286bf9d4217578cc8a4b98f7ec
miller <mr.oleg.melnyk@gmail.com>
2018-04-04 22:31:25 +0300

    Creating simple spring-boot project with web tools for developing RESTfull webapi service

---
+++ backend/src/main/java/com/se34/coursemap/CoursemapApplication.java
@@ -0,0 +1,12 @@
+package com.se34.coursemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemapApplication.class, args);
+	}
+}
