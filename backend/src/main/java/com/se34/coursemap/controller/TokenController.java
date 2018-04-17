1273b33916f432fab9ffccf5ed6b0dcaf75414ca
miller <mr.oleg.melnyk@gmail.com>
2018-04-18 01:40:54 +0300

    Implementing base classes of Jwt-Authorisation for secure accessing resources

---
+++ backend/src/main/java/com/se34/coursemap/controller/TokenController.java
@@ -0,0 +1,27 @@
+package com.se34.coursemap.controller;
+
+import com.se34.coursemap.model.JwtUser;
+import com.se34.coursemap.security.JwtGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
